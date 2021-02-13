cabce290c325c431c7f8505e1d4b2b184dbce95e
Aisyah Zahrotul Hidayah <43850912+hydaai@users.noreply.github.com>
2021-02-13 14:55:37 +0700

    Update Script.sql

--- Data Engineer Challenge with SQL/Script.sql
+++ Data Engineer Challenge with SQL/Script.sql
@@ -38,16 +38,34 @@ ORDER BY REPLACE(nama_pelanggan,'Ir. ','')
 SELECT
     nama_pelanggan
 FROM ms_pelanggan
+WHERE LENGTH(nama_pelanggan) = (SELECT MAX(LENGTH(nama_pelanggan)) FROM ms_pelanggan)
+
+/* -jika software yang digunakan mysql, gunakan length
+   -jika software yang digunakan sql server, ganti length menjadi len
+SELECT
+    nama_pelanggan
+FROM ms_pelanggan
 WHERE LEN(nama_pelanggan) = (SELECT MAX(LEN(nama_pelanggan)) FROM ms_pelanggan)
+*/
 
 /*---------------- Nama Pelanggan yang Paling Panjang dengan Gelar ----------------*/
 /*Instruksi : -Tampilkan nama pelanggan yang mempunyai nama terpanjang dan terpendek*/ 
 SELECT
     nama_pelanggan
 FROM ms_pelanggan
+WHERE LENGTH(nama_pelanggan) = (SELECT MAX(LENGTH(nama_pelanggan)) FROM ms_pelanggan)
+OR LENGTH(nama_pelanggan) = (SELECT MIN(LENGTH(nama_pelanggan)) FROM ms_pelanggan)
+ORDER BY LEN(nama_pelanggan) desc
+
+/* -jika software yang digunakan mysql, gunakan length
+   -jika software yang digunakan sql server, ganti length menjadi len
+SELECT
+    nama_pelanggan
+FROM ms_pelanggan
 WHERE LEN(nama_pelanggan) = (SELECT MAX(LEN(nama_pelanggan)) FROM ms_pelanggan)
 OR LEN(nama_pelanggan) = (SELECT MIN(LEN(nama_pelanggan)) FROM ms_pelanggan)
 ORDER BY LEN(nama_pelanggan) desc
+*/
 
 /*---------------- Kuantitas Produk yang Banyak Terjual ----------------*/
 /*Instruksi : -Tampilkan produk yang paling laris*/ 
@@ -58,12 +76,45 @@ SELECT
 FROM ms_produk a
 JOIN tr_penjualan_detail b on a.kode_produk = b.kode_produk 
 GROUP BY a.kode_produk, a.nama_produk
+HAVING sum(b.qty) = (SELECT SUM(qty) FROM tr_penjualan_detail 
+                    GROUP BY kode_produk 
+                    ORDER BY SUM(qty) DESC) 
+                    limit 1)
+
+/* -jika software yang digunakan mysql, gunakan limit
+   -jika software yang digunakan sql server, ganti limit diakhir menjadi top diawal
+SELECT 
+    a.kode_produk,
+    a.nama_produk,
+    sum(b.qty) as total_qty
+FROM ms_produk a
+JOIN tr_penjualan_detail b on a.kode_produk = b.kode_produk 
+GROUP BY a.kode_produk, a.nama_produk
 HAVING sum(b.qty) = (SELECT top 1 SUM(qty) FROM tr_penjualan_detail 
                     GROUP BY kode_produk 
                     ORDER BY SUM(qty) DESC)
+*/
 
 /*---------------- Pelanggan Paling Tinggi Nilai Belanjanya ----------------*/
 /*Instruksi : -Tampilkan pelanggan yang belanjanya paling besar*/
+SELECT 
+    a.kode_pelanggan,
+    a.nama_pelanggan,
+    sum(c.harga_satuan*c.qty) total_harga
+FROM ms_pelanggan a
+JOIN tr_penjualan b ON a.kode_pelanggan = b.kode_pelanggan 
+JOIN tr_penjualan_detail c ON b.kode_transaksi = c.kode_transaksi
+GROUP BY a.kode_pelanggan, a.nama_pelanggan
+HAVING sum(c.harga_satuan*c.qty) = 
+    (SELECT SUM(c.qty*c.harga_satuan) FROM ms_pelanggan a
+    JOIN tr_penjualan b ON a.kode_pelanggan = b.kode_pelanggan 
+    JOIN tr_penjualan_detail c ON b.kode_transaksi = c.kode_transaksi
+    GROUP BY a.kode_pelanggan 
+    ORDER BY SUM(c.qty*c.harga_satuan) DESC) 
+    limit 1)
+
+/* -jika software yang digunakan mysql, gunakan limit
+   -jika software yang digunakan sql server, ganti limit diakhir menjadi top diawal
 SELECT 
     a.kode_pelanggan,
     a.nama_pelanggan,
@@ -78,6 +129,7 @@ HAVING sum(c.harga_satuan*c.qty) =
     JOIN tr_penjualan_detail c ON b.kode_transaksi = c.kode_transaksi
     GROUP BY a.kode_pelanggan 
     ORDER BY SUM(c.qty*c.harga_satuan) DESC)
+*/
 
 /*---------------- Pelanggan yang Belum Pernah Berbelanja ----------------*/
 /*Instruksi : -Tampilkan pelanggan yang belum pernah melakukan transaksi*/ 
